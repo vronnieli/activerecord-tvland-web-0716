9b9345bef73500f1e020c2ddb8843b09a84a7422
vronnieli <vronnieli@gmail.com>
2016-08-10 13:36:06 -0400

    first draft

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,27 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+
+    character_name_array = []
+    self.characters.each do |character|
+      character_name_array << character.name
+    end
+
+    show_name_array =[]
+    self.shows.each do |show|
+      show_name_array << show.name
+    end
+
+    var = character_name_array.zip show_name_array
+    var.join(" - ")
+    
+  end
+
 end
